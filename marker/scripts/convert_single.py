d698a6bea1d45e9c747738cf2f5ef26562df2196
Ervin <ervincas13@gmail.com>
2025-09-30 05:27:07 +0000

    Revert change
    Signed-off-by: Ervin <ervincas13@gmail.com>

--- marker/scripts/convert_single.py
+++ marker/scripts/convert_single.py
@@ -13,12 +13,11 @@ from marker.config.parser import ConfigParser
 from marker.config.printer import CustomClickPrinter
 from marker.logger import configure_logging, get_logger
 from marker.models import create_model_dict
-from marker.output import save_output
 
-# import sys
-# from pathlib import Path
-# pdf_parsing_root = Path(__file__).resolve().parents[3]
-# sys.path.insert(0, str(pdf_parsing_root))
+import sys
+from pathlib import Path
+pdf_parsing_root = Path(__file__).resolve().parents[3]
+sys.path.insert(0, str(pdf_parsing_root))
 
 from parsers.tree import Tree
 from parsers.treeparser import TreeParser
